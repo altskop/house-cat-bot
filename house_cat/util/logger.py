00ff546b1957cd1756175ca9c16d26062488994c
altskop <altskop@gmail.com>
2019-07-10 16:18:44 -0500

    Fix logging issue where private messages would throw an exception

--- house_cat/util/logger.py
+++ house_cat/util/logger.py
@@ -4,13 +4,19 @@ logging.basicConfig(format='[%(asctime)s|%(levelname)s|%(guild)s|%(user)s] %(mes
 logger = logging.getLogger('house-cat')
 
 
+def get_guild_name(guild):
+    if guild is None:
+        return "private"
+    return guild.name
+
+
 def warn(ctx, message):
-    logger.warning(message, extra={'guild': ctx.guild.name, 'user': str(ctx.author.name+ctx.author.discriminator)})
+    logger.warning(message, extra={'guild': get_guild_name(ctx.guild), 'user': str(ctx.author.name+ctx.author.discriminator)})
 
 
 def debug(ctx, message):
-    logger.debug(message, extra={'guild': ctx.guild.name, 'user': str(ctx.author.name+ctx.author.discriminator)})
+    logger.debug(message, extra={'guild': get_guild_name(ctx.guild), 'user': str(ctx.author.name+ctx.author.discriminator)})
 
 
 def info(ctx, message):
-    logger.info(message, extra={'guild': ctx.guild.name, 'user': str(ctx.author.name+ctx.author.discriminator)})
+    logger.info(message, extra={'guild': get_guild_name(ctx.guild), 'user': str(ctx.author.name+ctx.author.discriminator)})
