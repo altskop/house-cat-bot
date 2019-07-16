e1feb3ca3542d81e6a980d58e3c8bd62bd2e18fc
altskop <altskop@gmail.com>
2019-07-16 17:45:31 -0500

    Add help message

--- house_cat/house_cat.py
+++ house_cat/house_cat.py
@@ -1,6 +1,4 @@
 import discord.ext.commands as commands
-from responses import response_builder as resp
-from voice import voice as vc
 from meme_generator.meme_gen_cog import MemeGeneratorCog
 from responses.response_cog import ResponseCog
 from games.game_cog import GameCog
@@ -12,11 +10,7 @@ class HouseCatBot(commands.Bot):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.config = {}
         self.color = 0x5297d5
-        # self.db = database_handler.DBHandler("../storage/db/discord_data.db")
-        self.responseBuilder = resp.ResponseBuilder()
-        self.voice = vc.Voice(self)
 
     async def on_ready(self):
         print('Logged in as')
@@ -28,6 +22,7 @@ class HouseCatBot(commands.Bot):
 logger = logging.getLogger('house-cat')
 logger.setLevel(logging.DEBUG)
 bot = HouseCatBot(command_prefix='$')
+bot.remove_command('help')
 bot.add_cog(MemeGeneratorCog(bot))
 bot.add_cog(ResponseCog(bot))
 bot.add_cog(GameCog(bot))
